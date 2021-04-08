b471ebfa6d1786cdce3d111c150037a4e9eda50c
Zver <an>
2021-04-08 14:07:47 +0300

    add to Git

---
+++ src/app/api-keys.ts
@@ -0,0 +1,11 @@
+export const masterFirebaseConfig = {
+  apiKey: "0cb9a93d5ab13a225189e1dd9afbdba9",
+  authDomain: "developer.firebaseapp.com/",
+  databaseURL: "https://developer.edamam.com",
+  projectId: "cc0320c5",
+  storageBucket: "edamam.appspot.com",
+  messagingSenderId: "12345"
+}
+
+export const recipeApiAppId = "cc0320c5";
+export const recipeApiKey = "0cb9a93d5ab13a225189e1dd9afbdba9";
